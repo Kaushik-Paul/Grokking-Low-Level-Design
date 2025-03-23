7123669e52d9271fe12dcdf7e7de9264a2ed67b0
kaushik <kaushik.paul755@gmail.com>
2025-03-23 13:30:40 +0530

    Update parking lot

--- src/com/example/lowleveldesign/parkinglot/vehicle/Car.java
+++ src/com/example/lowleveldesign/parkinglot/vehicle/Car.java
@@ -1,5 +1,7 @@
 package com.example.lowleveldesign.parkinglot.vehicle;
 
+import com.example.lowleveldesign.parkinglot.parkinglot.ParkingTicket;
+
 public class Car extends Vehicle {
     @Override
     public void assignTicket(ParkingTicket ticket) {
